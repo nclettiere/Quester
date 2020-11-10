efefa3a99bd405a5b3c312bb9fa2b5c6c5c4cb84
Nicolas Lettiere <braum.exe@gmail.com>
2020-11-10 04:13:01 -0300

    Basic ImGui Implementation

--- src/internal/private/QuestEditor.cpp
+++ src/internal/private/QuestEditor.cpp
@@ -3,5 +3,201 @@
 QuestEditor::QuestEditor()
     : wxFrame(NULL, wxID_ANY, "Quest Editor", wxDefaultPosition, wxSize(1024, 720), wxDEFAULT_FRAME_STYLE)
 {
-    
+    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+
+    int args[] = { WX_GL_RGBA, WX_GL_DOUBLEBUFFER, WX_GL_DEPTH_SIZE, 16, 0 };
+
+    glPane = new BasicGLPane((wxFrame*)this, args);
+
+    sizer->Add(glPane, 1, wxEXPAND);
+
+    this->SetSizer(sizer);
+    this->SetAutoLayout(true);
+
+    this->Show();
+}
+
+BEGIN_EVENT_TABLE(BasicGLPane, wxGLCanvas)
+EVT_MOTION(BasicGLPane::mouseMoved)
+EVT_LEFT_DOWN(BasicGLPane::mouseDown)
+EVT_LEFT_UP(BasicGLPane::mouseReleased)
+EVT_RIGHT_DOWN(BasicGLPane::rightClick)
+EVT_LEAVE_WINDOW(BasicGLPane::mouseLeftWindow)
+EVT_SIZE(BasicGLPane::resized)
+EVT_KEY_DOWN(BasicGLPane::keyPressed)
+EVT_KEY_UP(BasicGLPane::keyReleased)
+EVT_MOUSEWHEEL(BasicGLPane::mouseWheelMoved)
+EVT_PAINT(BasicGLPane::render)
+END_EVENT_TABLE()
+
+
+// some useful events to use
+void BasicGLPane::mouseMoved(wxMouseEvent& event) {}
+void BasicGLPane::mouseDown(wxMouseEvent& event) {}
+void BasicGLPane::mouseWheelMoved(wxMouseEvent& event) {}
+void BasicGLPane::mouseReleased(wxMouseEvent& event) {}
+void BasicGLPane::rightClick(wxMouseEvent& event) {}
+void BasicGLPane::mouseLeftWindow(wxMouseEvent& event) {}
+void BasicGLPane::keyPressed(wxKeyEvent& event) {}
+void BasicGLPane::keyReleased(wxKeyEvent& event) {}
+
+// Vertices and faces of a simple cube to demonstrate 3D render
+// source: http://www.opengl.org/resources/code/samples/glut_examples/examples/cube.c
+GLfloat v[8][3];
+GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
+    {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
+    {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
+
+
+
+BasicGLPane::BasicGLPane(wxFrame* parent, int* args) :
+    wxGLCanvas(parent, wxID_ANY, args, wxDefaultPosition, wxDefaultSize, wxFULL_REPAINT_ON_RESIZE)
+{
+    glsl_version = "#version 130";
+
+    m_context = new wxGLContext(this);
+    // prepare a simple cube to demonstrate 3D render
+    // source: http://www.opengl.org/resources/code/samples/glut_examples/examples/cube.c
+    v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
+    v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
+    v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
+    v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
+    v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
+    v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
+
+    // To avoid flashing on MSW
+    SetBackgroundStyle(wxBG_STYLE_CUSTOM);
+}
+
+BasicGLPane::~BasicGLPane()
+{
+    delete m_context;
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
+}
+
+void BasicGLPane::resized(wxSizeEvent& evt)
+{
+    //	wxGLCanvas::OnSize(evt);
+
+    Refresh();
+}
+
+/** Inits the OpenGL viewport for drawing in 3D. */
+void BasicGLPane::prepare3DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y)
+{
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black Background
+    glClearDepth(1.0f);	// Depth Buffer Setup
+    glEnable(GL_DEPTH_TEST); // Enables Depth Testing
+    glDepthFunc(GL_LEQUAL); // The Type Of Depth Testing To Do
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    glEnable(GL_COLOR_MATERIAL);
+
+    glViewport(topleft_x, topleft_y, bottomrigth_x - topleft_x, bottomrigth_y - topleft_y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    float ratio_w_h = (float)(bottomrigth_x - topleft_x) / (float)(bottomrigth_y - topleft_y);
+    gluPerspective(45 /*view angle*/, ratio_w_h, 0.1 /*clip close*/, 200 /*clip far*/);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        auto s = reinterpret_cast<const char*>(glewGetErrorString(err));
+        std::cout << "Error: " << s << std::endl;
+        wxLogMessage("Failed to initialize OpenGL loader!\n\n%s", s);
+        this->Destroy();
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
+    //ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+}
+
+/** Inits the OpenGL viewport for drawing in 2D. */
+void BasicGLPane::prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black Background
+    glEnable(GL_TEXTURE_2D);   // textures
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_BLEND);
+    glDisable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glViewport(topleft_x, topleft_y, bottomrigth_x - topleft_x, bottomrigth_y - topleft_y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluOrtho2D(topleft_x, bottomrigth_x, bottomrigth_y, topleft_y);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int BasicGLPane::getWidth()
+{
+    return GetSize().x;
+}
+
+int BasicGLPane::getHeight()
+{
+    return GetSize().y;
+}
+
+
+void BasicGLPane::render(wxPaintEvent& evt)
+{
+    if (!IsShown()) return;
+
+    wxGLCanvas::SetCurrent(*m_context);
+    //wxPaintDC(this); // only to be used in paint events. use wxClientDC to paint outside the paint event
+
+    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // ------------- draw some 3D ----------------
+    prepare3DViewport(getWidth(), 0, getWidth(), getHeight());
+    glLoadIdentity();
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        //glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, getWidth(), getHeight());
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        //glfwSwapBuffers(window);
+
+        SwapBuffers();
+        //glFlush();
+    //SwapBuffers();
 }
--- src/internal/public/QuestEditor.h
+++ src/internal/public/QuestEditor.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <stdio.h>
+
+// include OpenGL
+#ifdef __WXMAC__
+#include "OpenGL/glu.h"
+#include "OpenGL/gl.h"
+#else
+#include <GL/glew.h>
+#include <GL/gl.h>
+#endif
+
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
@@ -7,11 +21,20 @@
 #include <wx/sizer.h>
 #include <wx/listctrl.h>
 
-#include <GL/glew.h>
+#include "wx/sizer.h"
+#include "wx/glcanvas.h"
+
+#include "BasicGLPane.h"
+
+#include <iostream>
 
 class QuestEditor : public wxFrame
 {
 public:
 	QuestEditor();
+
+private:
+	wxFrame* frame;
+	BasicGLPane* glPane;
 };
 
